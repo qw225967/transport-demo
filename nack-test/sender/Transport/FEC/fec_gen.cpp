1e2c84eca7e8e8e206106cdcb3d83c19b9b02bae
qw225967 <396139919@qq.com>
2022-03-08 17:04:27 +0800

    start fec

---
+++ nack-test/sender/Transport/FEC/fec_gen.cpp
@@ -0,0 +1,9 @@
+/*******************************************************
+ * @author      : dog head
+ * @date        : Created in 2022/3/8 4:39 下午
+ * @mail        : qw225967@github.com
+ * @project     : nack_test
+ * @file        : fec_gen.cpp
+ * @description : TODO
+ *******************************************************/
+
---
+++ nack-test/sender/Transport/FEC/fec_gen.h
@@ -0,0 +1,14 @@
+/*******************************************************
+ * @author      : dog head
+ * @date        : Created in 2022/3/8 4:39 下午
+ * @mail        : qw225967@github.com
+ * @project     : nack_test
+ * @file        : fec_gen.h
+ * @description : TODO
+ *******************************************************/
+
+
+#ifndef NACK_TEST_FEC_GEN_H
+#define NACK_TEST_FEC_GEN_H
+
+#endif //NACK_TEST_FEC_GEN_H
